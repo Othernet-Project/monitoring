9b39e62b87507d8a6c562506067cd12050a0fe92
Andrean Franc <andrean.franc@gmail.com>
2015-11-27 15:26:13 +0100

    Ignore reported timestamps and instead attempt approximating them

--- monitoring/data/heartbeat.py
+++ monitoring/data/heartbeat.py
@@ -7,6 +7,9 @@ import time
 from bottle import request
 
 
+HEARTBEAT_PERIOD = 60  # should be same as in monitoring client
+
+
 def service_ok(lock, bitrate, transfers):
     if not lock:
         # This is always OK because if there's no lock, we don't know if the
@@ -23,27 +26,34 @@ def service_ok(lock, bitrate, transfers):
 
 def process_data(country_code, ip_addr, data):
     db = request.db.main
-
-    status = service_ok(data['signal_lock'], data['bitrate'],
-                        data['transfers'])
-
-    payload = {
-        'ip': ip_addr,
-        'location': country_code,
-        'client_id': data['client_id'],
-        'service_id': data['service_id'],
-        'pid': data['pid'],
-        'signal_lock': data['signal_lock'],
-        'bitrate': data['bitrate'],
-        'snr': data['snr'],
-        'transfers': data['transfers'],
-        'sat_config': data['sat_config'],
-        'service_ok': status,
-        'timestamp': data['timestamp'],
-        'processing_time': data['processing_time'],
-        'reported': time.time(),
-    }
-
-    qry = db.Insert('stats', cols=payload.keys())
-    db.execute(qry, payload)
-    return status
+    received_at = time.time()
+    # reversed because last entry is the newest one
+    for (idx, entry) in enumerate(reversed(data)):
+        # approximate timestamp based on time of heartbeat reception by
+        # subtracting the same interval that the client application uses from
+        # the reception time, making it:
+        # 0th entry ~= now
+        # 1st entry ~= now - HEARTBEAT_PERIOD
+        # 2nd entry ~= now - 2 * HEARTBEAT_PERIOD
+        timestamp = received_at - idx * HEARTBEAT_PERIOD
+        status = service_ok(entry['signal_lock'],
+                            entry['bitrate'],
+                            entry['transfers'])
+        payload = {
+            'ip': ip_addr,
+            'location': country_code,
+            'client_id': entry['client_id'],
+            'service_id': entry['service_id'],
+            'pid': entry['pid'],
+            'signal_lock': entry['signal_lock'],
+            'bitrate': entry['bitrate'],
+            'snr': entry['snr'],
+            'transfers': entry['transfers'],
+            'sat_config': entry['sat_config'],
+            'service_ok': status,
+            'timestamp': timestamp,
+            'processing_time': entry['processing_time'],
+            'reported': time.time(),
+        }
+        qry = db.Insert('stats', cols=payload.keys())
+        db.execute(qry, payload)

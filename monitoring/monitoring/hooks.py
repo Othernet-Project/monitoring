43bf28537aa3bf1df07357960c60101fe1a0e0f9
Manish Mathai <manishpmathai@gmail.com>
2015-12-10 17:06:46 +0530

    Port previous reporting to current data

--- monitoring/monitoring/hooks.py
+++ monitoring/monitoring/hooks.py
@@ -4,12 +4,15 @@ from .reporting import send_report
 
 
 def initialize(supervisor):
+    config = supervisor.config
     # setup geoip as an extension
-    data_path = supervisor.config['data.geoip']
+    data_path = config['data.geoip']
     supervisor.exts.geoip = GeoIP.open(data_path, GeoIP.GEOIP_MEMORY_CACHE)
     # schedule periodic report sending
-    report_interval = supervisor.config['reporting.interval']
+    report_interval = config['reporting.interval']
     supervisor.exts.tasks.schedule(send_report,
                                    args=(supervisor,),
                                    periodic=True,
                                    delay=report_interval)
+
+    config['last_check'] = 0

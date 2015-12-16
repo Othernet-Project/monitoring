2de279a2f048e1ee4a158eff474c5ef6e5bfe1e7
Manish Mathai <manishpmathai@gmail.com>
2015-12-16 18:50:37 +0530

    Set reporting interval and immediately schedule report

--- monitoring/monitoring/hooks.py
+++ monitoring/monitoring/hooks.py
@@ -4,10 +4,8 @@ from .reporting import send_report
 def initialize(supervisor):
     config = supervisor.config
     # schedule periodic report sending
-    report_interval = config['reporting.interval']
     supervisor.exts.tasks.schedule(send_report,
                                    args=(supervisor,),
-                                   periodic=True,
-                                   delay=report_interval)
+                                   periodic=True)
 
     config['last_check'] = 0

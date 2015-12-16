4764a3e16e640cea798a13b01d7ab588cc07befc
Manish Mathai <manishpmathai@gmail.com>
2015-12-16 21:25:19 +0530

    Schedule immediate report generation at startup

--- monitoring/monitoring/hooks.py
+++ monitoring/monitoring/hooks.py
@@ -3,9 +3,15 @@ from .reporting import send_report
 
 def initialize(supervisor):
     config = supervisor.config
+
+    report_interval = config['reporting.interval']
+    # schedule an immediate report sending
+    supervisor.exts.tasks.schedule(send_report, args=(supervisor,))
+
     # schedule periodic report sending
     supervisor.exts.tasks.schedule(send_report,
                                    args=(supervisor,),
-                                   periodic=True)
+                                   periodic=True,
+                                   delay=report_interval)
 
     config['last_check'] = 0

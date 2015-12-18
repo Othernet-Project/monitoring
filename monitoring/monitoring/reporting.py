04c49ccfff10fbcc82a2c0cdc41a0f5c7b583168
Manish Mathai <manishpmathai@gmail.com>
2015-12-18 15:16:36 +0530

    Aggregate email reports per satellite

--- monitoring/monitoring/reporting.py
+++ monitoring/monitoring/reporting.py
@@ -17,6 +17,11 @@ NOTIFICATION_INTERVAL = 2 * 60 * 60
 # Interval for which faulty signal from client is considered ok
 SIGNAL_OK_INTERVAL = 20 * 60
 
+# Statuses used for email alerts and dashboard
+STATUS_NORMAL = 'NORMAL'
+STATUS_WARNING = 'WARNING'
+STATUS_CRITICAL = 'CRITICAL'
+
 
 class ClientError(object):
     WARNING = 1
@@ -64,9 +69,9 @@ def get_sat_reports(db, interval=DATAPOINTS_INTERVAL):
     # not have a lock. This is intentional. If there is no lock, we can't
     # really assume anything about the signal, so it does not make sense to
     # claim unlocked signal is bad.
-    qry = db.Select('*', 'stats', where=['reported >= %(reported)s', 'signal_lock = True'],
+    qry = db.Select('*', 'stats', where=['reported >= %(reported)s'],
                     order=['tuner_preset', 'client_id', 'timestamp'])
-    return db.fetchall(qry, { 'reported': time_bracket })
+    return db.fetchall(qry, {'reported': time_bracket})
 
 
 def by_sat(results):
@@ -88,6 +93,7 @@ def check_ok(datapoints):
 
     failures_count = 0
     datapoints_count = 0
+    datapoints = itertools.ifilter(lambda d: d['signal_lock'], datapoints)
     for d in datapoints:
         datapoints_count += 1
         if d['carousels_count'] > 0 and any(d['carousels_status']):
@@ -112,7 +118,7 @@ def check_no_carousels(datapoints):
     failures_count = 0
     # Only look at the last 10 minutes worth of data to ensure that older
     # datapoints do not incorrectly influence the failure rate calculation
-    datapoints = itertools.ifilter(lambda d: (
+    datapoints = itertools.ifilter(lambda d: d['signal_lock'] and (
         time.time() - d['timestamp']) <= 600, datapoints)
     for d in datapoints:
         datapoints_count += 1
@@ -136,6 +142,7 @@ def check_bad_bitrate(datapoints):
 
     datapoints_count = 0
     failures_count = 0
+    datapoints = itertools.ifilter(lambda d: d['signal_lock'], datapoints)
     for d in datapoints:
         datapoints_count += 1
         bitrate = d['bitrate']
@@ -158,7 +165,7 @@ def check_no_service_lock(datapoints):
     datapoints_count = 0
     # Only look at the last 10 minutes worth of data to ensure that older
     # datapoints do not incorrectly influence the failure rate calculation
-    datapoints = itertools.ifilter(lambda d: (
+    datapoints = itertools.ifilter(lambda d: d['signal_lock'] and (
         time.time() - d['timestamp'] <= 600), datapoints)
     for d in datapoints:
         datapoints_count += 1
@@ -171,7 +178,7 @@ def check_no_service_lock(datapoints):
 HEALTH_OK = 'ok'
 HEALTH_NO_CAROUSELS = 'no_carousels'
 HEALTH_BAD_BITRATE = 'bad_bitrate'
-HEALTH_NO_SERVICE_LOCK = 'no_lock'
+HEALTH_NO_SERVICE_LOCK = 'no_service_lock'
 HEALTH_UNKNOWN = 'unknown'
 
 
@@ -196,28 +203,32 @@ def client_report(results):
     """
     health = HEALTH_OK
     results = list(results)
-    logging.debug('Found {} rows for client {}'.format(len(results), results['client_id']))
-    logging.debug('Starting with {}'.format(health))
+
     while health != HEALTH_UNKNOWN:
         transition_fn, next_state = health_transition_map[health]
         if not transition_fn(results):
-            logging.debug('Moving from {} to {}'.format(health, next_state))
             health = next_state
         else:
-            print('Confirmed {}'.format(health))
             break
 
     datapoints_count = len(results)
     total_bitrate = sum([r['bitrate'] for r in results])
     total_errors = len(filter(lambda r: not r['service_ok'], results))
+    total_no_signal_locks = len(filter(lambda r: not['signal_lock'], results))
 
     avg_bitrate = total_bitrate / (datapoints_count or 1)
     error_rate = total_errors / (datapoints_count or 1)
+    no_signal_lock_rate = total_no_signal_locks / (datapoints_count or 1)
     if health == HEALTH_OK:
         status = True
     elif health == HEALTH_UNKNOWN:
-        # This is a bit flaky and maybe a bad heuristic
-        status = (error_rate < 0.5)
+        # If less than 20% of datapoints show no service locks,
+        # then the client probably is ok
+        if no_signal_lock_rate < 0.2:
+            status = True
+        else:
+            # This is a bit flaky and maybe a bad heuristic
+            status = (error_rate < 0.5)
     else:
         status = False
     return health, error_rate, avg_bitrate, status
@@ -231,45 +242,52 @@ def error_block(title, errors):
     return msg
 
 
-def construct_message(errors):
-    critical = []
+def construct_message(sat_status):
+    criticals = []
     warnings = []
 
-    for e in errors:
+    for e in sat_status['errors']:
         if e.severity == e.CRITICAL:
-            critical.append(e)
+            criticals.append(e)
         else:
             warnings.append(e)
 
     msg = ''
-    if critical:
-        msg += error_block('CRITICAL ALERTS', critical)
+    msg += 'SATELLITE STATUS: {}\n\n'.format(sat_status['alert_status'])
+
+    if criticals:
+        msg += error_block('CRITICAL ALERTS', criticals)
     if warnings:
-        msg += error_block('WARNIGNS', warnings)
-    if not critical and not warnings:
-        msg += 'OPERATIONAL AGAIN.\n\n'
+        msg += error_block('WARNINGS', warnings)
     return msg
 
 
-def get_state(errors):
-    if any([e.severity == e.CRITICAL for e in errors]):
-        return 'CRITICAL'
-    if any([e.severity == e.WARNING for e in errors]):
-        return 'WARNING'
-    return 'OPERATIONAL'
+def get_state(sat_status):
+    if sat_status:
+        error_rate = sat_status['error_rate']
+        if error_rate > 0.1:
+            return STATUS_CRITICAL
+        elif error_rate > 0.05:
+            return STATUS_WARNING
+        else:
+            return STATUS_NORMAL
+    return STATUS_NORMAL
 
 
 def get_changed_states(sat_errors, config):
-    default = dict((p, 'OPERATIONAL') for p in get_preset_ids())
+    default = dict((p, STATUS_NORMAL) for p in get_preset_ids())
     old_state = config.get('last_state', default)
     # prepare current state of sat_id:status pairs
-    state = dict((p, get_state(sat_errors.get(p, [])))
+    state = dict((p, get_state(sat_errors.get(p, None)))
                  for p in get_preset_ids())
     # find difference between current and previous state
     changes = dict(set(old_state.items()).difference(state.items()))
     config['last_state'] = state
     # return changed sat_id:errors pairs (empty error list if it works again)
-    return dict((key, sat_errors.get(key, [])) for key in changes)
+    changed_states = dict((key, sat_errors.get(key)) for key in changes)
+    for preset in changes:
+        changed_states[preset]['alert_status'] = state.get(preset, STATUS_NORMAL)
+    return changed_states
 
 
 def send_reports(sat_errors, config):
@@ -280,39 +298,25 @@ def send_reports(sat_errors, config):
     smtp_pass = config['email.password']
     recipients = config['reporting.recipients']
     c = SMTPClient(smtp_host, smtp_port, smtp_ssl, smtp_user, smtp_pass)
-    for preset, errors in sat_errors.items():
+    for preset, sat_status in sat_errors.items():
         sat_name = get_sat_name(preset)
         subject = '[OUTERNET MONITOR ALERT] {}'.format(sat_name)
-        message = construct_message(errors)
+        message = construct_message(sat_status)
         try:
             c.send(recipients, subject, message)
         except Exception:
             logging.exception("Mail report sending failed.")
 
 
-def aggregate_status(sat_status):
+def aggregate_status(sat_status, sat_states):
     aggregate = {}
     for sat_name, data in sat_status.items():
-        nconfigs = len(data)
-        nclients = sum([d['clients'] for d in data]) or 1
-        all_errors = list(itertools.chain(*[d['errors'] for d in data]))
-
-        fcritical = sum([e.severity == e.CRITICAL for e in all_errors]) \
-            / nclients
-        fwarnings = sum([e.severity == e.WARNING for e in all_errors]) \
-            / nclients
-        if fcritical > 0.1:
-            status = 'CRITICAL'
-        elif fwarnings > 0.1:
-            status = 'WARNINGS'
-        else:
-            status = 'NORMAL'
-
+        status = sat_states.get(data['preset'], STATUS_NORMAL)
         aggregate[sat_name] = {
             'status': status,
-            'clients': nclients,
-            'error_rate': sum([d['error_rate'] for d in data]) / nconfigs,
-            'bitrate': sum([d['bitrate'] for d in data]) / nconfigs
+            'clients': data['nclients'],
+            'error_rate': data['error_rate'],
+            'bitrate': data['bitrate']
         }
 
     return aggregate
@@ -322,7 +326,6 @@ def send_report(supervisor):
     app = supervisor.app
     config = app.config
 
-    error_threshold = config['reporting.error_rate_threshold']
     datapoints_interval = config['reporting.datapoints_interval']
 
     db = supervisor.exts.databases['monitoring']
@@ -336,7 +339,6 @@ def send_report(supervisor):
     for tuner_preset, sat_reports in reports_by_sat:
 
         clients = 0
-        total_error_rate = 0
         total_bitrate = 0
 
         errors = []
@@ -346,23 +348,26 @@ def send_report(supervisor):
             clients += 1
             health, errate, avg_bitrate, status = client_report(client_reports)
             total_bitrate += avg_bitrate
-            if not status and errate > error_threshold:
+            if not status:
                 errors.append(HighErrorRate(client_id, health, errate))
 
         sat_name = get_sat_name(tuner_preset)
-        sat_status.setdefault(sat_name, [])
-        sat_status[sat_name].append({
-            'errors': errors,
-            'error_rate': total_error_rate / (clients or 1),
+        sat_error_rate = len(errors) / (clients or 1)
+        sat_status.setdefault(sat_name, {})
+        sat_status[sat_name] = {
+            'preset': tuner_preset,
+            'error_rate': sat_error_rate,
             'bitrate': total_bitrate / (clients or 1),
-            'clients': clients})
+            'nclients': clients,
+            'errors': errors
+        }
 
         if errors:
-            sat_errors[tuner_preset] = errors
+            sat_errors[tuner_preset] = sat_status[sat_name]
 
     changes = get_changed_states(sat_errors, config)
     if changes:
         send_reports(changes, config)
 
-    config['last_report'] = aggregate_status(sat_status)
+    config['last_report'] = aggregate_status(sat_status, config['last_state'])
     config['last_check'] = time.time()
